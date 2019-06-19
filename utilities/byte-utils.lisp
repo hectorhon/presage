c9012ecc8d8e19091975a34424a786d080e26d94
hectorhon <hon.lianhung@gmail.com>
2019-06-20 01:11:05 +0800

    Use vector to store bytes

--- utilities/byte-utils.lisp
+++ utilities/byte-utils.lisp
@@ -1,18 +1,22 @@
 (in-package :com.hon.utils.byte)
 
 (defun integer-to-bytes (integer bits)
-  (loop :for pos :downfrom (- bits 8) :to 0 :by 8
-     :collecting (ldb (byte 8 pos) integer)))
+  (loop :with byte-count = (/ bits 8)
+     :with arr = (make-array byte-count :element-type 'unsigned-byte)
+     :for i :from 0 :below byte-count
+     :do (setf (aref arr i)
+               (ldb (byte 8 (* (- (1- byte-count) i) 8)) integer))
+     :finally (return arr)))
 
 (check-equals "integer-to-bytes"
-              '(0 0 1 1)
+              #(0 0 1 1)
               (integer-to-bytes 257 32))
 
 (defun bytes-to-integer (bytes)
-  (loop :for byte :in (coerce bytes 'list)
+  (loop :for byte :across bytes
      :for pos :downfrom (1- (length bytes))
      :summing (ash byte (* pos 8))))
 
 (check-equals "bytes-to-integer"
               65792
-              (bytes-to-integer '(0 0 1 1 0)))
+              (bytes-to-integer #(0 0 1 1 0)))
--- utilities/test-utils.lisp
+++ utilities/test-utils.lisp
@@ -4,10 +4,23 @@
   "Checks that the arguments are #'equal."
   `(funcall (lambda (expected actual)
               (progn (format t "TEST ~a..." ,description)
-                     (if (equal expected actual)
-                         (format t " OK.~%")
-                         (progn (format t " Failed. Expected ~a but got ~a instead.~%"
-                                        expected actual)
-                                (error "Expected ~a but got ~a instead."
-                                       expected actual)))))
+                     (cond ((and (typep expected 'vector)
+                                 (typep actual 'vector))
+                            (if (eql (length expected) (length actual))
+                                (loop :for i :from 0 :below (length actual)
+                                   :for actual-elem = (aref actual i)
+                                   :and expected-elem = (aref expected i)
+                                   :unless (equal actual-elem expected-elem)
+                                   :do (progn (format t "Failed. Expected ~a but got ~a instead (index ~d).~%"
+                                                      expected actual i)
+                                              (error "Expected ~a but got ~a instead (index ~d)."
+                                                     expected actual i)))
+                                (format t "OK.~%")))
+                           ((equal expected actual)
+                            (format t " OK.~%"))
+                           (t
+                            (format t " Failed. Expected ~a but got ~a instead.~%"
+                                    expected actual)
+                            (error "Expected ~a but got ~a instead."
+                                   expected actual)))))
             ,expected ,actual))
