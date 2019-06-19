f9eaa93972382f57bbb8ecc61dfdb16150716d39
hectorhon <hon.lianhung@gmail.com>
2019-06-19 12:21:19 +0800

    log-debug should be a macro
    - so that arguments are not evaluated when the log level is higher
    - so that can show *package*
    - so that can filter by package

--- utilities/logging.lisp
+++ utilities/logging.lisp
@@ -1,13 +1,18 @@
 (in-package :com.hon.utils.logging)
 
 (let ((log-stream *standard-output*))
-  (defun log-debug (control-string &rest format-arguments)
-    (let ((log-entry
-           (with-output-to-string (log-entry)
-             (apply #'format log-entry control-string
-                    format-arguments))))
-      (format log-stream "DEBUG: ~a~%"
-              ;;              log-entry))))
-              (replace-all log-entry "
-" "
-       ")))))
+  (defun log-stream ()
+    log-stream))
+
+(defmacro log-debug (control-string &rest format-arguments)
+  `(let ((log-entry
+          (format nil ,control-string ,@format-arguments))
+         (prefix
+          (concatenate 'string "DEBUG "
+                       ;; (package-name ,*package*)
+                       )))
+     (format (log-stream) "~a: ~a~%"
+             prefix
+             (replace-all log-entry "
+" (concatenate 'string "
+" (make-string (+ (length prefix) 2) :initial-element #\Space))))))
